31487de67ca18627ff359ab4f4b3c2b8ae37335b
Mitchell Kotler <mitch@mitchellkotler.net>
2022-03-18 16:49:54 -0400

    add alerting

--- main.py
+++ main.py
@@ -14,11 +14,16 @@ from documentcloud.addon import CronAddOn
 from documentcloud.constants import BULK_LIMIT
 from documentcloud.toolbox import grouper
 
+# the site to scrape
 SITE = "https://www.ssa.gov/foia/readingroom.html"
+# the project to upload documents into
 PROJECT = 207338
+# keywords to generate additional notifications for
+KEYWORDS = ["court", "foia"]
 
 
 def title(url):
+    """Get the base name of the file to use as a title"""
     parsed_url = urlparse.urlparse(url)
     basename = os.path.basename(parsed_url.path)
     root, _ext = os.path.splitext(basename)
@@ -27,6 +32,7 @@ def title(url):
 
 # https://stackoverflow.com/questions/33049729/how-to-handle-links-containing-space-between-them-in-python
 def url_fix(s):
+    """Fixes quoting of characters in file names to use with requests"""
     scheme, netloc, path, qs, anchor = urlparse.urlsplit(s)
     path = urlparse.quote(path, "/%")
     qs = urlparse.quote_plus(qs, ":&=")
@@ -34,8 +40,8 @@ def url_fix(s):
 
 
 class Scraper(CronAddOn):
-
     def load_data(self):
+        """Load data from the file checked into the repository"""
         try:
             with open("data.json", "r") as file_:
                 return json.load(file_)
@@ -43,10 +49,12 @@ class Scraper(CronAddOn):
             return {}
 
     def store_data(self, data):
+        """Store data to be checked in to the repository"""
         with open("data.json", "w") as file_:
             json.dump(data, file_, indent=2, sort_keys=True)
 
-    def main(self):
+    def scrape(self):
+        """Scrape the site for new documents"""
         resp = requests.get(SITE)
         resp.raise_for_status()
         soup = BeautifulSoup(resp.text, "html.parser")
@@ -66,10 +74,10 @@ class Scraper(CronAddOn):
                 data[doc]["last_seen"] = now
 
         print(f"Found {len(docs)} new documents")
+        self.send_mail(f"Found {len(docs)} new documents from {SITE}", "\n".join(docs))
         for doc_group in grouper(docs, BULK_LIMIT):
-            doc_group = [
-                d for d in doc_group if d
-            ]  # filter out None's from grouper padding
+            # filter out None's from grouper padding
+            doc_group = [d for d in doc_group if d]
             doc_group = [
                 {
                     "file_url": url_fix(d),
@@ -83,6 +91,27 @@ class Scraper(CronAddOn):
             resp = self.client.post("documents/", json=doc_group)
 
         self.store_data(data)
+
+    def alert(self):
+        """Run queries for the keywords to generate additional alerts"""
+        for keyword in KEYWORDS:
+            query = f"+project:{PROJECT} {keyword} created_at:[NOW-1HOUR TO *]"
+            documents = self.client.documents.search(query)
+            documents = list(documents)
+            if documents:
+                message = [
+                    f"Documents containing {keyword} found at {datetime.now()} "
+                    f"from {SITE}"
+                ]
+                message.extend([f"{d.title} - {d.canonical_url}" for d in documents])
+                self.send_mail(
+                    f"New documents found for: {keyword} from {SITE}",
+                    "\n".join(message),
+                )
+
+    def main(self):
+        self.scrape()
+        self.alert()
 
 
 if __name__ == "__main__":
